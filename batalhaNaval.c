be601e39f88a5eb9a15d562b03f0bd575ce1d2d8
Gabriel Duraky <gabrielmourabatistagmb10@gmail.com>
2025-09-09 23:05:12 +0000

    Implementação de habilidades

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
+// coodernadas habilidade cruz (2, 3), matriz [3][3]
+// coodernadas octaedro 
+#define TABULEIRO_LINHAS 10
+#define TABULEIRO_COLUNAS 10
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+
+void habilidade(int tabuleiro[TABULEIRO_LINHAS][TABULEIRO_COLUNAS], 
+     int habilidade_matriz[3][5], int linha_inicio, int coluna_inicio) { // declaração das variaveis da função
+        for(int i = 0; i < 3; i++) { // for para linhas da habilidade_matriz
+            for(int j = 0; j < 5; j++) { // for para colunas da habiliade_matriz
+                if (habilidade_matriz[i][j] != 0) { // verifica se o valor do indice não é zero
+                    int tabuleiro_linha = linha_inicio + i;
+                    int tabuleiro_coluna = coluna_inicio + j;
+                    if (tabuleiro_linha < TABULEIRO_LINHAS && tabuleiro_coluna < TABULEIRO_COLUNAS) {
+                        if (tabuleiro[tabuleiro_linha][tabuleiro_coluna] == 0) {
+                            tabuleiro[tabuleiro_linha][tabuleiro_coluna] = habilidade_matriz[i][j];
+                        }
+                    }
+                }
+            }
+        }
+     }
 
 int main() {
    
-    int tabuleiro[10][10] = {0}; // construção da matriz com todos os slots com 0
-    int navio1_linha = 6, navio1_coluna = 7; // navio na horizontal
-    int navio2_linha = 4, navio2_coluna = 5; // navio na vertical
+    int tabuleiro[TABULEIRO_LINHAS][TABULEIRO_COLUNAS] = {0}; // construção da matriz com todos os slots com 0
+    int navio1_linha = 6, navio1_coluna = 8; // navio na vertical
+    int navio2_linha = 4, navio2_coluna = 5; // navio na horizontal
     int navio3_linha = 1, navio3_coluna = 6; // navio na diagonal vetor baixo/direita
     int navio4_linha = 8, navio4_coluna = 2; // navio na diagonal vetor cima/ direita
+    int cruz[3][5] = {{0, 0, 1, 0, 0}, {1, 1, 1, 1, 1}, {0, 0, 1, 0, 0}};
+    int octaedro[3][5] = {{0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}};
+    int cone[3][5] = {{0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}};
 
-    for( int i = 0; i < 3; i++) { // preenchimento dos slots vazios em 3 unidades no eixo x/y
-        tabuleiro[navio1_linha + i][navio1_coluna] = 3; // navio na horizontal
-        tabuleiro[navio2_linha][navio2_coluna + i] = 3; // navio na vertical
+    for(int i = 0; i < 3; i++) { // preenchimento dos slots vazios em 3 unidades no eixo x/y
+        tabuleiro[navio1_linha + i][navio1_coluna] = 3; // navio vertical
+        tabuleiro[navio2_linha][navio2_coluna + i] = 3; // navio na horizontal
         tabuleiro[navio3_linha + i][navio3_coluna + i] = 3; // navio diagonal para baixo/direita
         tabuleiro[navio4_linha - i][navio4_coluna + i] = 3; // navio  diagonal para cima/direita
     }
+
+    habilidade(tabuleiro, cruz, 0, 1); // 
+    habilidade(tabuleiro, octaedro, 4, 0);
+    habilidade(tabuleiro, cone, 6, 3);
+
+    
 
     // saida do tabuleiro
     printf("Tabuleiro:\n"); 
